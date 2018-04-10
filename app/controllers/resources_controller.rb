74af2824da952945866938bb77fe8a47ff222c10
smithwebtek <brad@smithwebtek.com>
2018-04-10 08:45:42 -0400

    Cloudinary resources working

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,44 +1,38 @@
 class ResourcesController < ApplicationController
-	class DemoController < ApplicationController
 
-		def index
-			res = Cloudinary::Api.resources(resource: 'image', format: 'pdf', max_results: 500)
-			@resources = res['resources']
-			render 'demo/resources'
-		end
-		
-		def new 
-			render 'demo/new'
-		end
-		
-		def show 
-			
-			render 'show'
-		end
-	
-		def new_upload
-			file = params["file"]
-			Cloudinary::Uploader.upload(file, :resource_type => :image,
-			:public_id => params['file'].original_filename,
-			:chunk_size => 6_000_000
-			)
-			@resource = file
-			redirect_to 'show'
-		end
-		
-		def pdfs
-			res = Cloudinary::Api.resources(resource: 'image', format: 'pdf', max_results: 500)
-			@pdfs = res['resources'].select{|r|r["format"]== 'pdf'}
-			render 'demo/pdfs'
-		end
-	
-		private
-	
-		def local_image_path(name)
-			Rails.root.join('uploads', name).to_s
-		end
-	
+	def index
+		res = Cloudinary::Api.resources(resource: 'image', format: 'pdf', max_results: 500)
+		@resources = res['resources']
+		render 'resources/resources'
 	end
 	
+	def new 
+		render 'resources/new'
+	end
+	
+	def show 
+		
+		render 'show'
+	end
 
+	def new_upload
+		file = params["file"]
+		Cloudinary::Uploader.upload(file, :resource_type => :image,
+		:public_id => params['file'].original_filename,
+		:chunk_size => 6_000_000
+		)
+		@resource = file
+		redirect_to 'show'
+	end
+	
+	def pdfs
+		res = Cloudinary::Api.resources(resource: 'image', format: 'pdf', max_results: 500)
+		@pdfs = res['resources'].select{|r|r["format"]== 'pdf'}
+		render 'resources/pdfs'
+	end
+
+	private
+	def local_image_path(name)
+		Rails.root.join('uploads', name).to_s
+	end
 end
